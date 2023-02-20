05df4fb748eeb8e2a4a9ff61dd80dbd55439b404
rbamarc <rbamarc929@gmail.com>
2023-02-20 02:24:02 -0800

    change to random number generator

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class App {
@@ -6,19 +7,20 @@ public class App {
         //declare the number of variables and initialize to 0
         int answer = 0;
         int guess = 0;
+        Random rng = new Random();
 
         //declare and initialize a scanner object
         Scanner myScanner = new Scanner(System.in);
 
-        //ask player 1 to enter a number from 1 to 20
-        System.out.println("Player1: please enter a number between 1 and 20");
-        //now wait until a number is entered
-        answer = myScanner.nextInt();
-        //note there should be an error check here to make sure an int was entered
+        //generate random number between 1 and 20
+        System.out.println("GENERATING RANDOM NUMBER! BEEP BOP BOOP");
+        
+        answer = rng.nextInt(20) + 1;
+        
         // now start the guessing game!
         while ( true ){
             //get a guess
-            System.out.println("Player 2: please guess a number between 1 and 20");
+            System.out.println("HUMAN! please guess a number between 1 and 20");
             guess = myScanner.nextInt();
 
             //does guess equal answer?
@@ -36,6 +38,6 @@ public class App {
             }
         
         }
-        System.out.println("That's it! The number was: " + answer);
+        System.out.println("....YOU ARE SAFE FOR NOW.... The number was: " + answer);
     }
 }
